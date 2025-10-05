c3c2e4381bf1a1c5af53f9834162cbe6e3aa20b3
Juliano Machado <102674195+julianomachadoo@users.noreply.github.com>
2025-10-04 21:45:17 -0300

    Update ssh_config.sh

--- ssh_config.sh
+++ ssh_config.sh
@@ -9,6 +9,4 @@ fi
 ssh-keygen -t rsa -b 4096 -C "seu_email@exemplo.com" -f ./id_rsa_custom
 
 # Copia a chave pública para o servidor remoto
-#ssh-copy-id -i ./id_rsa_custom.pub ${ANSIBLE_USER}@${VM_IP}
-
 cat ./id_rsa_custom.pub | ssh -i ./id_rsa_custom ${ANSIBLE_USER}@${VM_IP} "mkdir -p ~/.ssh && cat >> ~/.ssh/authorized_keys && chmod 600 ~/.ssh/authorized_keys && chmod 700 ~/.ssh"
